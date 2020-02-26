2ce4e10a7976eca59ed66ab218a748c4b5bdea20
rajesha <rajesha@fortunapix.com>
2020-02-26 16:44:32 +0530

    updated

--- chart/animation.js
+++ chart/animation.js
@@ -3,6 +3,7 @@ let nodes;
 let groupKeys;
 let countOfgroup;
 let statusOfOverdue;
+let statusOfCompleted;
 window.onload = function () {
     
     OrgChart.templates.group_grey_new = Object.assign({}, OrgChart.templates.group_grey);
@@ -11,17 +12,17 @@ window.onload = function () {
 
     OrgChart.templates.group_grey_new.defs = '<filter x="-50%" y="-50%" width="200%" height="200%" filterUnits="objectBoundingBox" id="cool-shadow"><feOffset result="offOut" in="SourceGraphic" dx="0" dy="0" /><feColorMatrix result = "matrixOut" in = "offOut" type = "matrix" values = "0.05     0     0     0     0 0     0.64     0     0     0 0     0     0.5     0     0 0     0     0     1     0 "/><feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="10" /><feBlend in="SourceGraphic" in2="blurOut" mode="normal" /></filter>';
 
-    OrgChart.templates.group_grey_new.node = '<rect rx="12" x="12" y="12" filter="url(#cool-shadow)" height="{h}" width="{w}" fill="#ffffff" stroke-width="0" stroke="#aeaeae"></rect>';
+    OrgChart.templates.group_grey_new.node = '<rect rx="12" x="12" y="12" filter="url(#cool-shadow)" height="{h}" width="{w}" fill="#F9FBFC" stroke-width="0" stroke="#aeaeae"></rect>';
 
 
 
     OrgChart.templates.group_red_new.defs = '<filter x="-50%" y="-50%" width="200%" height="200%" filterUnits="objectBoundingBox" id="red-shadow"><feOffset result="offOut" in="SourceGraphic" dx="0" dy="0" /><feColorMatrix result = "matrixOut" in = "offOut" type = "matrix" values = "0.82     0     0     0     0 0     0.13     0     0     0 0     0     0.18     0     0 0     0     0     1     0"/><feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="10" /><feBlend in="SourceGraphic" in2="blurOut" mode="normal" /></filter>';
 
-    OrgChart.templates.group_red_new.node = '<rect rx="12" x="12" y="12" filter="url(#red-shadow)" height="{h}" width="{w}" fill="#ffffff" stroke-width="0" stroke="#aeaeae"></rect>';
+    OrgChart.templates.group_red_new.node = '<rect rx="12" x="12" y="12" filter="url(#red-shadow)" height="{h}" width="{w}" fill="#F9FBFC" stroke-width="0" stroke="#aeaeae"></rect>';
 
     OrgChart.templates.node_custom.defs = '<filter x="-50%" y="-50%" width="200%" height="200%" filterUnits="objectBoundingBox" id="custom-shadow"><feOffset dx="0" dy="4" in="SourceAlpha" result="shadowOffsetOuter1" /><feGaussianBlur stdDeviation="10" in="shadowOffsetOuter1" result="shadowBlurOuter1" /><feColorMatrix values="0 0 0 0 0   0 0 0 0 0    0 0 0 0  0 0 0 0.5 0" in="shadowBlurOuter1" type="matrix" result="shadowMatrixOuter1" /><feMerge><feMergeNode in="shadowMatrixOuter1" /><feMergeNode in="SourceGraphic" /></feMerge></filter>';
 
-    OrgChart.templates.node_custom.node = '<rect rx="12" x="12" y="12" filter="url(#custom-shadow)" height="{h}" width="{w}" fill="#ffffff" stroke-width="0" stroke="#aeaeae"></rect><line x1="20" y1="50" x2="255" y2="50" stroke="#AFAFAF" stroke-width="0.5" ></line><text  style="font-size: 15px;" fill="#696767" x="30" y="96">Assigned to :</text>';
+    OrgChart.templates.node_custom.node = '<rect rx="12" x="12" y="12" filter="url(#custom-shadow)" height="{h}" width="{w}" fill="#F9FBFC" stroke-width="0" stroke="#aeaeae"></rect><line x1="20" y1="50" x2="255" y2="50" stroke="#AFAFAF" stroke-width="0.5" ></line><text  style="font-size: 15px;" fill="#696767" x="30" y="96">Assigned to :</text>';
 
     //nodes data
     OrgChart.templates.node_custom.typeOfInt = '<text  style="font-size: 17px;" fill="#000000" x="30" y="36">{val}</text>';
@@ -55,104 +56,104 @@ window.onload = function () {
     // }
 
 
- nodes = [{
-            id: 1,
-            tags: ["Ui"],
-            typeOfInt: "Intigration",
-            assignDate: "17th Jan 2020",
-            assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
-            assignedTo: "Rajesh",
-            status: "Overdue",
-            startDate: "",
-            endDate:""
-        },
-        {
-            id: 2,
-            tags: ["Ui"],
-            typeOfInt: "Intigration",
-            assignDate: "17th Jan 2020",
-            assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
-            assignedTo: "Rajesh",
-            status: "Overdue",
-            startDate: "",
-            endDate:""
-        },
-        {
-            id: 3,
-            tags: ["Ui"],
-            typeOfInt: "Intigration",
-            assignDate: "17th Jan 2020",
-            assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
-            assignedTo: "Rajesh",
-            status: "Completed",
-            startDate: "",
-            endDate:""
-        },
+    nodes = [{
+        id: 1,
+        tags: ["Ui"],
+        typeOfInt: "Intigration",
+        assignDate: "17th Jan 2020",
+        assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
+        assignedTo: "Rajesh",
+        status: "Overdue",
+        startDate: "",
+        endDate:""
+    },
+    {
+        id: 2,
+        tags: ["Ui"],
+        typeOfInt: "Intigration",
+        assignDate: "17th Jan 2020",
+        assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
+        assignedTo: "Rajesh",
+        status: "Overdue",
+        startDate: "",
+        endDate:""
+    },
+    {
+        id: 3,
+        tags: ["Ui"],
+        typeOfInt: "Intigration",
+        assignDate: "17th Jan 2020",
+        assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
+        assignedTo: "Rajesh",
+        status: "Completed",
+        startDate: "",
+        endDate:""
+    },
 
-        {
-            id: 4,
-            tags: [],
-            pid: 1,
-            typeOfInt: "Intigration",
-            assignDate: "17th Jan 2020",
-            assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
-            assignedTo: "Rajesh",
-            status: "Completed",
-            startDate: "",
-            endDate:""
-        },
+    {
+        id: 4,
+        tags: [],
+        pid: 1,
+        typeOfInt: "Intigration",
+        assignDate: "17th Jan 2020",
+        assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
+        assignedTo: "Rajesh",
+        status: "Completed",
+        startDate: "",
+        endDate:""
+    },
 
-        {
-            id: 5,
-            tags: [],
-            pid: 1,
-            tags: ["Algorithms"],
-            typeOfInt: "Intigration",
-            assignDate: "17th Jan 2020",
-            assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
-            assignedTo: "Rajesh",
-            status: "",
-            startDate: "",
-            endDate:""
-        },
-        {
-            id: 6,
-            tags: [],
-            pid: 1,
-            tags: ["Algorithms"],
-            typeOfInt: "Intigration",
-            assignDate: "17th Jan 2020",
-            assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
-            assignedTo: "Rajesh",
-            status: "Completed",
-            startDate: "",
-            endDate:""
-        },
+    {
+        id: 5,
+        tags: [],
+        pid: 1,
+        tags: ["Algorithms"],
+        typeOfInt: "Intigration",
+        assignDate: "17th Jan 2020",
+        assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
+        assignedTo: "Rajesh",
+        status: "Not Started",
+        startDate: "",
+        endDate:""
+    },
+    {
+        id: 6,
+        tags: [],
+        pid: 1,
+        tags: ["Algorithms"],
+        typeOfInt: "Intigration",
+        assignDate: "17th Jan 2020",
+        assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
+        assignedTo: "Rajesh",
+        status: "Completed",
+        startDate: "",
+        endDate:""
+    },
 
-        {
-            id: 7,
-            tags: [],
-            pid: 1,
-            tags: ["Algorithms"],
-            typeOfInt: "Intigration",
-            assignDate: "17th Jan 2020",
-            assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
-            assignedTo: "Rajesh",
-            status: "Overdue",
-            startDate: "",
-            endDate:""
+    {
+        id: 7,
+        tags: [],
+        pid: 1,
+        tags: ["Algorithms"],
+        typeOfInt: "Intigration",
+        assignDate: "17th Jan 2020",
+        assignImg: "https://icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png",
+        assignedTo: "Rajesh",
+        status: "Overdue",
+        startDate: "",
+        endDate:""
 
-        }
-        
-    ]
-
-
+    }
+    
+]
 
 
 
 
 
-    OrgChart.REMOVE_GROUP_IF_HAS_ONE_NODE = true;
+
+
+    OrgChart.REMOVE_GROUP_IF_HAS_ONE_NODE = false;
     chart = new OrgChart(document.getElementById("tree"), {
         //scaleInitial: OrgChart.match.boundary,
         enableSearch: false,
@@ -192,22 +193,14 @@ window.onload = function () {
             Ui: {
                 group: true,
                 groupName: "Ui",
-                groupState: OrgChart.EXPAND,
+                groupState: OrgChart.COLLAPSE,
                 template: "group_grey_new"
             },
             Algorithms: {
                 group: true,
                 groupName: "Algorithms",
                 groupState: OrgChart.COLLAPSE,
-
                 template: "group_red_new",
-                nodeBinding: {
-                    typeOfInt: "typeOfInt",
-                    assignData: "assignDate",
-                    assignImg: "assignImg",
-                    assignedTo: "assignedTo",
-                    field_number_children: "field_number_children"
-                }
             },
         },
         nodes: nodes
@@ -221,39 +214,51 @@ window.onload = function () {
 
     chart.on('redraw', function () {
         groupKeys = Object.keys(chart.config.tags);
-        //groupStatus = Object.
         groupKeys.forEach((ele, i)=>{
-            countOfgroup =  nodes.filter(ele => ele.tags[0] == groupKeys[i]).length;
-            //debugger
-            statusOfOverdue = nodes.filter(ele => ele.status == "Overdue").length;
-            groupInNodes = nodes.filter((ele)=>{
-                return ele.tags[0] == groupKeys[i]
-            })
-            //console.log(groupInNodes[0])
-            for (let i = 0; i < groupInNodes.length; i++) {
-                console.log(groupKeys)
-            }
-            $('#'+ele).append('<svg id="groupNode">'+
+            countOfgroup =  nodes.filter(ele => ele.tags[0] == groupKeys[i]);
+            statusOfOverdue = countOfgroup.filter(ele => ele.status == "Overdue");
+            statusOfCompleted = countOfgroup.filter(ele => ele.status == "Completed");
+            statusOfNotStarted = countOfgroup.filter(ele => ele.status == "Not Started");
+            $('#'+ele).append('<svg class="groupnode">'+
             '<circle cx="34" cy="64" r="12" fill="#F57C00"></circle>'+
-            '<text fill="#ffffff" x="34" y="70" text-anchor="middle">' + countOfgroup + '</text>'+
+            '<text fill="#ffffff" x="34" y="70" text-anchor="middle">' + countOfgroup.length + '</text>'+
             '<text  fill="#000000" x="50" y="70" font-family="" font-size="14">All Tasks</text>'+
             
             '<circle cx="160" cy="64" r="12" fill="#D2222D"></circle>'+
-            '<text fill="#ffffff" x="160" y="70" text-anchor="middle">' + countOfgroup + '</text>'+
+            '<text fill="#ffffff" x="160" y="70" text-anchor="middle">' + statusOfOverdue.length + '</text>'+
             '<text  fill="#D2222D" x="178" y="70" font-family="" font-size="14">Overdue</text>'+
             
             '<circle cx="34" cy="94" r="12" fill="#4CB09C"></circle>'+
-            '<text fill="#ffffff" x="34" y="100" text-anchor="middle">' + countOfgroup + '</text>'+
+            '<text fill="#ffffff" x="34" y="100" text-anchor="middle">' + statusOfCompleted.length + '</text>'+
             '<text  fill="#4CB09C" x="50" y="100" font-family="" font-size="14">Completed</text>'+
             
             '<circle cx="160" cy="94" r="12" fill="#AFAFAF"></circle>'+
-            '<text fill="#ffffff" x="160" y="100" text-anchor="middle">' + countOfgroup + '</text>'+
+            '<text fill="#ffffff" x="160" y="100" text-anchor="middle">' + statusOfNotStarted.length + '</text>'+
             '<text  fill="#AFAFAF" x="178" y="100" font-family="" font-size="14">Not Started</text>'+
             '<line x1="20" y1="50" x2="255" y2="50" stroke="#AFAFAF" stroke-width="0.5" ></line>'+
-            '<clipPath id="cut-off-bottom"><circle cx="138" cy="92" r="14" /></clipPath><image x="122" y="76" xlink:href="" height="32" width="32" clip-path="url(#cut-off-bottom)" ></image>'+
-            '</svg>')
-        })
+            '<svg class=groupnodeImg></svg></svg>');
+            countOfgroup.forEach((elem, j)=>{
+                if(j >= 3){
+                    $('#'+ele +' .groupnodeImg').append('<svg><circle cx="82" cy="117" r="14" fill="#ffffff"></circle><text  fill="#0000008D" x="74" y="122" font-family="" font-size="14">4+</text></svg>')
+                    j = elem.length;
+                    return;
+                }
+                $('#'+ele +' .groupnodeImg').append('<svg><clipPath id="cut-off-bottom_'+j+'"><circle cx='+(36 + j * 16)+' cy="117" r="14" /></clipPath><image x='+(20 + j * 16)+' y="100" xlink:href="'+elem.assignImg+'" height="32" width="32" clip-path="url(#cut-off-bottom_'+j+')" ></image></svg>')
+            })
+        });
+        
     });
+            chart.on('expcollclick', function (sender, action, id, ids) {
+                debugger
+                if(action === 0){
+                    //alert("open")
+                    $('#'+id+' .groupnode').remove()
+                }else {
+                    //alert("close")
+                }
+             });  
+
+    
     // chart.on('', function () {
 
     // }
@@ -273,3 +278,20 @@ window.onload = function () {
   };
 
 
+
+        // <text transform="matrix(1 0 0 1 788.5117 513.7266)" fill="#AFAFAF" font-family="'SegoeUI'" font-size="12">Not Started</text>
+        // <text transform="matrix(1 0 0 1 758.6816 549.541)" fill="#AFAFAF" font-family="'SegoeUI'" font-size="11">Working together</text>
+        // <text transform="matrix(1 0 0 1 689.8589 513.7266)" fill="#65AC9B" font-family="'SegoeUI'" font-size="12">Completed</text>
+        // <text transform="matrix(1 0 0 1 690.1411 480.1143)" fill="#3C3C3C" font-family="'SegoeUI-Bold'" font-size="14">All Tasks</text>
+        // <circle fill="#D2222D" cx="775.012" cy="475.179" r="8.5"/>
+        // <circle fill="#AFAFAF" cx="775.012" cy="509.093" r="8.5"/>
+        // <circle fill="none" stroke="#A1D6C7" stroke-miterlimit="10" cx="676.5" cy="475.179" r="8.5"/>
+        // <path fill="none" stroke="#00B49D" stroke-width="1.5" stroke-linecap="round" stroke-miterlimit="10" d="M668,475.179
+        //     c0-4.693,3.806-8.5,8.5-8.5"/>
+        // <circle fill="#65AC9B" cx="676.5" cy="509.093" r="8.5"/>
+        // <text transform="matrix(1 0 0 1 671.5 478.9688)" fill="#00B49D" font-family="'SegoeUI-Bold'" font-size="9">22</text>
+        // <text transform="matrix(1 0 0 1 670.667 512.3027)" fill="#FFFFFF" font-family="'SegoeUI-Bold'" font-size="9">11</text>
+        // <text transform="matrix(1 0 0 1 769.5 512.3027)" fill="#FFFFFF" font-family="'SegoeUI-Bold'" font-size="9">11</text>
+        
+        //     <image xlink:href="" type="image/png" width="60" height="26" transform="matrix(0.9999 0 0 0.9999 671.6665 533.542)">
+        // </image>
